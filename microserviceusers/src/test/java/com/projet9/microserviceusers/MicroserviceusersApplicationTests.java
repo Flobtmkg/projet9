f16f191624bd7bfe4e577028e064218d3356c92c
Florian Couturier <florian.couturier33@gmail.com>
2018-12-28 17:28:21 +0100

    Commit d'initialisation du projet9

---
+++ microserviceusers/src/test/java/com/projet9/microserviceusers/MicroserviceusersApplicationTests.java
@@ -0,0 +1,17 @@
+package com.projet9.microserviceusers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceusersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
