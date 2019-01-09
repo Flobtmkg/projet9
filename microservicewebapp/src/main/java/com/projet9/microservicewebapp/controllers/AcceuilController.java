211a74433b518e6e1c4db3df7caf97229934c26d
Florian Couturier <florian.couturier33@gmail.com>
2019-01-09 12:23:41 +0100

    Sprint1 -> Documentation
     - Mise en place de Swagger

--- microservicewebapp/src/main/java/com/projet9/microservicewebapp/controllers/AcceuilController.java
+++ microservicewebapp/src/main/java/com/projet9/microservicewebapp/controllers/AcceuilController.java
@@ -1,8 +1,13 @@
 package com.projet9.microservicewebapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AcceuilController {
 
+    @GetMapping("/")
+    public String accueil(){
+        return "accueil";
+    }
 }
