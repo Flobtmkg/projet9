0a8373fa3cf2edc6a39dff6a25980ac8b6dddaf9
Romain Camberbet-Hardoy <romain.camberbet@gmail.com>
2019-01-19 18:32:48 +0100

    Sprint 5 -> Config client Eureka pour tous les micro-services

--- microserviceaventures/src/main/java/com/projet9/microserviceaventures/MicroserviceaventuresApplication.java
+++ microserviceaventures/src/main/java/com/projet9/microserviceaventures/MicroserviceaventuresApplication.java
@@ -2,8 +2,10 @@ package com.projet9.microserviceaventures;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceaventuresApplication {
 
 	public static void main(String[] args) {
--- microservicereservations/src/main/java/com/projet9/microservicereservations/MicroservicereservationsApplication.java
+++ microservicereservations/src/main/java/com/projet9/microservicereservations/MicroservicereservationsApplication.java
@@ -2,8 +2,10 @@ package com.projet9.microservicereservations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroservicereservationsApplication {
 
 	public static void main(String[] args) {
