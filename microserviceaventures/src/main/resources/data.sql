f7d15612e507877faf27f4be0b00850d1e8fc4b4
Florian Couturier <florian.couturier33@gmail.com>
2019-02-23 19:35:28 +0100

    Sprint 10 -> Correction bug de lien de Thymeleaf
    - En insérant un commentaire entre les deux balises <a> le bug est contourné: Thymeleaf à visibleament du mal a parser les attributs "th:xxx" dans le fichier html quand deux balises du même type (ici <a>) se suivent mais utilisent deux façons différentes de générer l'élément. Ici <a href="Something"> suivie de <a th:href="Something">.
    Il suffit d'insérer un commentaire entre les deux pour résoudre le problème.

--- microserviceaventures/src/main/resources/data.sql
+++ microserviceaventures/src/main/resources/data.sql
@@ -2,5 +2,5 @@ INSERT INTO categorie (id, nom, description) VALUES (1, 'Mer', 'La mer c''est tr
 INSERT INTO categorie (id, nom, description) VALUES (2, 'Montagne', 'La montagne ça vous gagne !');
 
 INSERT INTO aventure (id, nom, prix, date_debut, date_fin, date_cloture, description, id_categorie, limit_reservation) VALUES (1, 'Aventure1', 100, '2019-02-19', '2019-02-23', '2019-01-19', 'C''est l''aventure 1 whouhou', 1, 50);
-INSERT INTO aventure (id, nom, prix, date_debut, date_fin, date_cloture, description, id_categorie, limit_reservation) VALUES (2, 'Aventure2', 200, '2019-03-06', '2019-03-13', '2019-02-23', 'C''est l''aventure 2 whaaaaa', 1, 45);
+INSERT INTO aventure (id, nom, prix, date_debut, date_fin, date_cloture, description, id_categorie, limit_reservation) VALUES (2, 'Aventure2', 200, '2019-04-06', '2019-04-13', '2019-4-23', 'C''est l''aventure 2 whaaaaa', 1, 45);
 INSERT INTO aventure (id, nom, prix, date_debut, date_fin, date_cloture, description, id_categorie, limit_reservation) VALUES (3, 'Aventure3', 300, '2019-01-14', '2019-01-24', '2019-01-03', 'C''est l''aventure 3 woooooh', 2, 28);
